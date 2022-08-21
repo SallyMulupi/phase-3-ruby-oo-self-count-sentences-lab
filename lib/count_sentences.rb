9f7e98dbd6bc26a4d6394e8e347fa6e44ec6109b
SallyMulupi <mulupisally2gmail.com>
2022-08-21 10:48:13 +0300

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self[-1] == '!'? true : false
   end
 
   def count_sentences
-
+    self.split(/[?!.]/).filter{|e| e != ''}.count
   end
 end
